77440931038c641c6eea2f3816e39dc96fe067d1
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2026-07-06 21:46:15 +0200

    fixed auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -15,4 +15,18 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+
+  //  1. The full URL of your production application.
+  // Better Auth uses this to configure cookie paths.
+  baseURL: 'https://inventory-management-app-pearl.vercel.app',
+
+  // 2. cookie settings for security and operation on subdomains.
+  cookie: {
+    secure: true,
+    sameSite: 'Lax', // Standard for preventing CSRF.
+    // We specify the domain so that the cookie is available on all paths.
+    domain: '.vercel.app',
+  },
+
+  // --------------------------
 });
