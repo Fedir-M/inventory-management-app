5c3bbb986b4c69380d1a7befe2d5551a7d74f6be
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2026-07-06 18:04:59 +0200

    fixed auth-client

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,8 +1,10 @@
 import { createAuthClient } from 'better-auth/react';
 
 // Init of front-client
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
-});
+// export const authClient = createAuthClient({
+//   baseURL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
+// });
+
+export const authClient = createAuthClient();
 
 export const { signIn, signUp, useSession } = authClient;
