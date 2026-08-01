1f93f8198d44c2e4cca6ab7b51ec7851553fe413
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2026-08-01 15:38:23 +0200

    fixture - navigation

--- e2e/specs/navigation.spec.ts
+++ e2e/specs/navigation.spec.ts
@@ -1,22 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../fixtures/test';
 
 test.describe('Authentication - Navigation Flow', () => {
   test('Should navigate between Sign-Up, Sign-In and Home pages correctly', async ({
+    signUpPage,
+    signInPage,
     page,
   }) => {
     // 1. Открываем страницу регистрации
-    await page.goto('/sign-up');
+    await signUpPage.open();
 
-    // 2. Кликаем по ссылке перехода на страницу входа
-    const signInLink = page.getByRole('link', {
-      name: /already have\? sign-in/i,
-    });
-    await signInLink.click();
+    // 2. Кликаем по ссылке перехода на страницу '/sign-in'
+    await signUpPage.signInLink.click();
     await expect(page).toHaveURL('/sign-in');
 
     // 3. Возвращаемся на главную страницу через ссылку "Back Home"
-    const backHomeLink = page.getByRole('link', { name: 'Back Home' });
-    await backHomeLink.click();
+    await signInPage.backHomeLink.click();
     await expect(page).toHaveURL('/');
   });
 });
